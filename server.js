75d7f24cad29ef1f7444aece0b931722de3c8576
milleymilli <milleyny@gmail.com>
2025-04-24 12:20:00 +0100

    trying to fix cors

--- server.js
+++ server.js
@@ -7,14 +7,20 @@ const path = require("path");
 const authenticateToken = require("./routes/auth");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const corsOptions = {
+  origin: "https://your-railway-backend-url.up.railway.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
+app.use(cors(corsOptions));
 // Middleware Configuration
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET", "POST"],
+//   })
+// );
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
